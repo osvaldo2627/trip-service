9e99010a2bbfbadc8d57cd14979ff5ca381f7c01
Osvaldo Aguilar Lauzurique <osvaldo.aguilar@share-now.com>
2019-12-30 14:08:32 +0100

    post method added

--- src/controller/customer-controller.js
+++ src/controller/customer-controller.js
@@ -8,4 +8,14 @@ module.exports = ({ router, CustomerService }) => {
     const records = await CustomerService.find()
     res.send(records)
   })
+
+  router.post('/customers', async (req, res) => {
+    const document = req.body
+    document.id = document.id || uuidv1()
+
+    const record = new Customer(document)
+    await record.save()
+
+    res.send(record)
+  })
 }
